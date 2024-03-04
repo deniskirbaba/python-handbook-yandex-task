4da1a4a02dc849d884222f62ebb7a47cf08d0ad3
deniskirbaba <kirbabadenis@gmail.com>
2024-03-05 00:43:03 +0300

    revise 4-1

--- 4-1-functions-scope-parameters/palindrome_7.py
+++ 4-1-functions-scope-parameters/palindrome_7.py
@@ -1,4 +1,4 @@
-def is_palindrome(obj):
+def is_palindrome(obj) -> bool:
     '''
     Takes the integer, string, tuple or list and check if this object is palindrome.
 
@@ -10,8 +10,6 @@ def is_palindrome(obj):
     '''
     if isinstance(obj, int):
         obj = str(obj)
-    elif isinstance(obj, (tuple, list)):
-        obj = ''.join([str(i) for i in obj])
     length_obj = len(obj)
     res = obj[:length_obj // 2] == obj[(length_obj + 1) // 2:][::-1]
     return res
--- 4-1-functions-scope-parameters/string_of_numbers.py
+++ 4-1-functions-scope-parameters/string_of_numbers.py
@@ -1,7 +1,13 @@
+# def split_numbers(s):
+#     numbers = []
+#     while (i := s.find(' ')) != -1:
+#         numbers.append(int(s[:i]))
+#         s = s[i + 1:]
+#     numbers.append(int(s))
+#     return tuple(numbers)
+
 def split_numbers(s):
-    numbers = []
-    while (i := s.find(' ')) != -1:
-        numbers.append(int(s[:i]))
-        s = s[i + 1:]
-    numbers.append(int(s))
-    return tuple(numbers)
+    return tuple([int(i) for i in s.split()])
+
+result = split_numbers("1 2 3 4 5")
+print(result)
