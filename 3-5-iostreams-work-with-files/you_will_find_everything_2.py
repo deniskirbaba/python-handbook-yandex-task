17249b553991b8ebc8827c4316d6cc311a0a903c
deniskirbaba <kirbabadenis@gmail.com>
2024-03-04 23:13:25 +0300

    revise 3-5

--- 3-5-iostreams-work-with-files/you_will_find_everything_2.py
+++ 3-5-iostreams-work-with-files/you_will_find_everything_2.py
@@ -1,7 +1,7 @@
 from sys import stdin
 
 page_titles = stdin.readlines()
-query = page_titles[-1].lower().rstrip('\n')
-page_titles = page_titles[:-1]
+query = page_titles.pop().lower().rstrip('\n')
+
 result_idxs = (idx for idx, title in enumerate(page_titles) if query in title.lower().rstrip('\n'))
 [print(page_titles[res_idx].rstrip('\n')) for res_idx in result_idxs]
