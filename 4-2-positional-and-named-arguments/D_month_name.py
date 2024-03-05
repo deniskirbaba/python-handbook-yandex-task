54a3e7e9824b7030a5574d7e09c3a272c02725f2
deniskirbaba <kirbabadenis@gmail.com>
2024-03-05 12:06:59 +0300

    revise 4-2

---
+++ 4-2-positional-and-named-arguments/D_month_name.py
@@ -0,0 +1,27 @@
+def month(n, lang='ru'):
+    months = {'ru': ['Январь', 
+                     'Февраль', 
+                     'Март', 
+                     'Апрель', 
+                     'Май', 
+                     'Июнь', 
+                     'Июль', 
+                     'Август', 
+                     'Сентябрь', 
+                     'Октябрь', 
+                     'Ноябрь', 
+                     'Декабрь'],
+              'en': ['January', 
+                     'February', 
+                     'March', 
+                     'April', 
+                     'May', 
+                     'June', 
+                     'July', 
+                     'August', 
+                     'September', 
+                     'October', 
+                     'Novebmer', 
+                     'December']}
+    return months[lang][n - 1]
+    
