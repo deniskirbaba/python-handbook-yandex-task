29b42e9661d215f42d26d88181597b0ca12e8962
deniskirbaba <kirbabadenis@gmail.com>
2024-03-07 22:11:35 +0300

    revise 6

--- 6-2-pandas/10_func_extrema.py
+++ 6-2-pandas/10_func_extrema.py
@@ -2,16 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def arange(start, stop, step=1, endpoint=True):
-    arr = np.arange(start, stop, step)
-    if endpoint and np.isclose(arr[-1] + step, stop):
-        arr = np.concatenate([arr, [arr[-1] + step]])
-
-    return arr
-
-
 def values(func, start, end, step):
-    x = arange(start, end, step, endpoint=True)
+    n = int(abs((end - start) / step)) + 1
+    x = np.linspace(start, end, num=n)
     return pd.Series(data=[func(i) for i in x], index=x)
 
 
@@ -23,7 +16,12 @@ def max_extremum(data: pd.Series):
     return data.idxmax()
 
 
-data = values(lambda x: x ** 2 + 2 * x + 1, -1.5, 1.7, 0.1)
-print(data)
-print(min_extremum(data))
-print(max_extremum(data))
+# Tests for values func
+print(values(lambda x: x ** 2 + 2 * x + 1, -1.5, 1.7, 0.1))
+print(values(lambda x: x ** 2 + 2 * x + 1, -1.5, 1.7, 2))
+print(values(lambda x: x ** 2 + 2 * x + 1, -1.5, 4.5, 2))
+
+# data = values(lambda x: x ** 2 + 2 * x + 1, -1.5, 1.7, 0.1)
+# print(data)
+# print(min_extremum(data))
+# print(max_extremum(data))
